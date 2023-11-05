86040c904f7ffd5fdb57faba6716da440522a475
Tomer Saada <tomer1594826@gmail.com>
2023-11-06 01:37:22 +0200

    asdf

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="w-60 hidden lg:block bg-gray-800 h-[calc(100vh-4rem)]">
-        <svg version="1.1" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="w-6 h-6">
-            <g><path d="M4 16V4H2v12h2zM13 15l-1.5-1.5L14 11H6V9h8l-2.5-2.5L13 5l5 5-5 5z"></path></g>
-        </svg>
+        <h1>chat</h1>
     </div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-16 flex w-full bg-gray-900 items-center text-white text-xl font-medium px-3">
-        <RouterLink to="/"><img src="logo5.jpg" class="w-12 rounded-full hover:scale-110 transition-all"></RouterLink>
+        <RouterLink to="/"><img src="/logo5.jpg" class="w-12 rounded-full hover:scale-110 transition-all"></RouterLink>
         <RouterLink v-for="route in routes.filter(route => route.path !== '/')" :to="route.path" class="mx-6">{{ route.name }}</RouterLink>
     </div>
 </template>
