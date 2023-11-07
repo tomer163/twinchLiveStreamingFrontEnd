d9fc89e40a501fd0e242ee1c1fa483c4b333631b
Tomer Saada <tomer1594826@gmail.com>
2023-11-07 14:12:46 +0200

    adfadf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="">
-        <div class="relative xl:block hidden grow mx-10 h-[30rem]">
+        <div class="relative hidden md:block mx-5 h-[30rem]">
             <button @click="goRight" class="absolute top-0 bottom-0 right-0 z-40">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                 </svg>
             </button>
-            <div v-for="(a,index) in array" :class="a" class="absolute top-0 bottom-0 left-0 right-0 m-auto h-[20rem] w-[48rem] transition-all duration-700">
+            <div v-for="(a,index) in array" :class="a" class="absolute top-0 bottom-0 left-0 right-0 m-auto h-[20rem] w-[48rem] transition-all duration-700 xl:shadow-[0_0px_50px_15px_rgb(0,0,0,0.7)]">
+                <div v-if="index === 2" class="bg-red-500 w-full h-full"></div>
             </div>
             <button @click="goLeft" class="absolute top-0 bottom-0 left-0 z-40">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -20,13 +21,15 @@
 <script setup>
 import { ref } from 'vue'
 
+
 const array = ref([
-    'bg-gray-700 scale-75 translate-x-[20rem] z-10',
-    'bg-gray-600 scale-90 translate-x-[10rem] z-20',
-    'bg-gray-500 z-30',
-    'bg-gray-600 scale-90 -translate-x-[10rem] z-20',
-    'bg-gray-700 scale-75 -translate-x-[20rem] z-10'
-])
+    `xl:translate-x-[14rem] 2xl:translate-x-[22rem] z-10 scale-75 bg-gray-700`,
+    `xl:translate-x-[8rem] 2xl:translate-x-[12rem] z-20 scale-90 bg-gray-600`,
+    `z-30 bg-gray-500`,
+    `xl:translate-x-[-8rem] 2xl:translate-x-[-12rem] z-20 scale-90 bg-gray-600`,
+    `xl:translate-x-[-14rem] 2xl:translate-x-[-22rem] z-10 scale-75 bg-gray-700`
+]);
+
 
 function goLeft(){
     array.value.splice(0,0,array.value.pop())
