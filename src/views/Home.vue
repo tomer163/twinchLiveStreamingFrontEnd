2bcf22fa5edb50f4960bcdb65bf0b83c6f5162b6
Tomer Saada <tomer1594826@gmail.com>
2023-11-07 02:08:33 +0200

    ype

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="relative xl:block hidden grow mx-10 h-[30rem]">
-        <button @click="goRight" class="absolute top-0 bottom-0 right-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
-        </button>
-        <div v-for="(a,index) in array" :class="a" class="absolute top-0 bottom-0 left-0 right-0 m-auto h-[20rem] w-[48rem] transition-all duration-1000">
+    <div class="">
+        <div class="relative xl:block hidden grow mx-10 h-[30rem]">
+            <button @click="goRight" class="absolute top-0 bottom-0 right-0 z-40">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+            </button>
+            <div v-for="(a,index) in array" :class="a" class="absolute top-0 bottom-0 left-0 right-0 m-auto h-[20rem] w-[48rem] transition-all duration-700">
+            </div>
+            <button @click="goLeft" class="absolute top-0 bottom-0 left-0 z-40">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                </svg>
+            </button>
         </div>
-        <button @click="goLeft" class="absolute top-0 bottom-0 left-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-            </svg>
-        </button>
     </div>
 </template>
 
